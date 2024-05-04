da5df05dea39b215a1727dfda32bc79070382f55
Mai_Hai <maisonhai3@gmail.com>
2024-05-04 04:07:30 +0000

    chore: Update landing page title and description

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Do's wedding!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="I promise we will have a wedding!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          That is a promise of <code>my life</code>!!
         </p>
       </main>
 
